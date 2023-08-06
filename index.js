0dd5200229b766b16c199286f22513b6c0cbe17c
Anton Zold <140329295+antonzoldtech@users.noreply.github.com>
2023-08-06 18:59:06 +0200

    added slideshow to home section and changed theme colours

--- index.js
+++ index.js
@@ -11,12 +11,20 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 const slideshow = document.querySelector(".slideshow");
+const slideshow2 = document.querySelector(".slideshow-2")
 
 setInterval(() => {
     const firstSlideshowImage = slideshow.firstElementChild;
+    console.log(firstSlideshowImage)
     slideshow.removeChild(firstSlideshowImage)
     slideshow.appendChild(firstSlideshowImage)
 }, 6000)
+
+setInterval(() => {
+    const firstSlideshowImage2 = slideshow2.firstElementChild;
+    slideshow2.removeChild(firstSlideshowImage2)
+    slideshow2.appendChild(firstSlideshowImage2)
+}, 8000)
 
 hamburger.addEventListener("click", () => {
     bar1.classList.toggle("animateBar1");
@@ -27,20 +35,23 @@ hamburger.addEventListener("click", () => {
 
 const blackPearl = getComputedStyle(root).getPropertyValue("--fixed-black-pearl")
 const white = getComputedStyle(root).getPropertyValue("--fixed-white")
-
+const topaz = getComputedStyle(root).getPropertyValue("--fixed-topaz")
 const whiteIce = getComputedStyle(root).getPropertyValue("--fixed-white-ice")
 const deepSea = getComputedStyle(root).getPropertyValue("--fixed-deep-sea")
-const slate = getComputedStyle(root).getPropertyValue("--fixed-slate")
+const dune = getComputedStyle(root).getPropertyValue("--fixed-dune")
+const onyx = getComputedStyle(root).getPropertyValue("--fixed-onyx")
+const geyser = getComputedStyle(root).getPropertyValue("--fixed-geyser")
 
 const  blackPearlFilter = getComputedStyle(document.querySelector(":root")).getPropertyValue("--fixed-black-pearl-filter");
 const whiteFilter = getComputedStyle(document.querySelector(":root")).getPropertyValue("--fixed-white-filter");
 
 darkModeSliderBar.addEventListener("click", function () {
     if (!darkModeSliderCircle.classList.contains("activateDarkMode")) {
-        root.style.setProperty("--theme-body-color", slate)
+        root.style.setProperty("--theme-body-color", dune)
         root.style.setProperty("--theme-font-color", white);
-        root.style.setProperty("--theme-overlay-color", blackPearl)
-        root.style.setProperty("--theme-nav-color", blackPearl)
+        root.style.setProperty("--theme-overlay-color", onyx)
+        root.style.setProperty("--theme-nav-color", onyx)
+        root.style.setProperty("--theme-accent-color", geyser)
         for (let i = 0; i < document.getElementsByClassName("icon-to-filter").length; i++) {
             document.getElementsByClassName("icon-to-filter").item(i).style.filter = whiteFilter
         }
@@ -50,7 +61,8 @@ darkModeSliderBar.addEventListener("click", function () {
         root.style.setProperty("--theme-body-color", white)
         root.style.setProperty("--theme-font-color", blackPearl);
         root.style.setProperty("--theme-overlay-color", white)
-        root.style.setProperty("--theme-nav-color", deepSea)
+        root.style.setProperty("--theme-nav-color", topaz)
+        root.style.setProperty("--theme-accent-color", topaz)
         for (let i = 0; i < document.getElementsByClassName("icon-to-filter").length; i++) {
             document.getElementsByClassName("icon-to-filter").item(i).style.filter = blackPearlFilter
         }
